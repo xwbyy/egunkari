4a3afa19a84cf2a8a3b5ba3b41155a68946cdd9b
xwby <160570513+xwbyy@users.noreply.github.com>
2025-04-12 19:06:30 +0700

    Update auth.js

--- public/auth.js
+++ public/auth.js
@@ -1,14 +1,13 @@
-// auth.js
 function loginWithGoogle() {
   try {
     const clientId = "1054339746495-1oiv1uf35qqcbjk63r1epda3s5ap7st8.apps.googleusercontent.com";
-    const redirectUri = encodeURIComponent("https://egunkarii.vercel.app/api/callback");
+    const redirectUri = encodeURIComponent(window.location.origin + "/api/callback");
     
     const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?` +
       `client_id=${clientId}` +
       `&redirect_uri=${redirectUri}` +
       `&response_type=code` +
-      `&scope=profile email` +
+      `&scope=profile%20email` +
       `&access_type=offline` +
       `&prompt=consent`;
     
