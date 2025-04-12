2b57dc5f98ccdc58f09a6921a375dbdcf2787ca2
xwby <160570513+xwbyy@users.noreply.github.com>
2025-04-12 14:01:11 +0700

    Add files via upload

--- public/auth.js
+++ public/auth.js
@@ -1,8 +1,12 @@
+// auth.js - untuk redirect user ke login Google
 function loginWithGoogle() {
   const clientId = "1054339746495-1oiv1uf35qqcbjk63r1epda3s5ap7st8.apps.googleusercontent.com";
   const redirectUri = "https://egunkarii.vercel.app/api/callback";
   const scope = "profile email";
   const responseType = "code";
+
   const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
+
+  // Redirect ke Google Login
   window.location.href = url;
 }
